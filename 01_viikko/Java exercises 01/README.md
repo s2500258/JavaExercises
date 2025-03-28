2b034e186681076892252fc9225434d968f569b8
s2500258 <akulov.alex.wrk@gmail.com>
2025-03-28 13:07:41 +0200

    1 week

---
+++ 01_viikko/Java exercises 01/README.md
@@ -0,0 +1,2 @@
+## Week 1 Exercises
+- we got introduced to syntax
---
+++ 01_viikko/Java exercises 01/skripti.js
@@ -0,0 +1,16 @@
+console.log("Tämä on JS testi");
+
+const muuttuja1="Hello World";
+
+function funktio() {
+    let muuttuja2="Let's start...";
+    let x, y, z;
+    x=1;
+    y=5;
+    z=x+y;
+    console.log ("Tulostettaan 1: ", muuttuja1);
+    console.log (muuttuja2);
+    console.log("Z =",z);
+}
+
+funktio();
